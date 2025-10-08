864d45dc202958c76d78147d352fbed950576526
debiangee1990 <luigigabrielbespiritu@iskolarngbayan.pup.edu.ph>
2025-10-08 22:00:53 +0800

    Create GetKeywordsPerLogs.c

---
+++ String Collector/GetKeywordsPerLogs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// LGBESPIRITU - Project to get the unique keywords on the downloaded logs of Jira
+// To be used in preparation of Jira Virtual Agent
+
+int main(){
+
+
+
+
+    return 0;
+}
